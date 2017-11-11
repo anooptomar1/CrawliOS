1554320b44766a7701535a853710a66d39d14c29
Jack Chorley <jack.chorley16@imperial.ac.uk>
2017-11-11 19:10:33 +0100

    Added some map code

--- Crawler/CrawlsViewController.swift
+++ Crawler/CrawlsViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  Crawler
+//  Crawl
 //
 //  Created by Jack Chorley on 11/11/2017.
 //  Copyright © 2017 Jack Chorley. All rights reserved.
@@ -8,10 +8,12 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 class CrawlsViewController: UIViewController, NSFetchedResultsControllerDelegate {
     
-    let shadowColor = UIColor.colorWithHsb(0, s: 0, b: 39, a: 0.12)
+    let shadowColor = UIColor.colorWithHsb(0, s: 0, b: 39, a: 0.5)
+    let shadowColorLight = UIColor.colorWithHsb(0, s: 0, b: 39, a: 0.3)
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -61,3 +63,44 @@ class CrawlsViewController: UIViewController, NSFetchedResultsControllerDelegate
 
 }
 
+
+extension CrawlsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crawlCell", for: indexPath) as! CrawlTableViewCell
+        
+        cell.cellView.layer.shadowPath = CGPath(roundedRect: cell.cellView.bounds, cornerWidth: 0, cornerHeight: 0, transform: nil)
+        cell.clipsToBounds = false
+        cell.cellView.clipsToBounds = false
+        cell.cellView.layer.shadowColor = shadowColorLight.cgColor
+        cell.cellView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        cell.cellView.layer.shadowRadius = 10
+        cell.cellView.layer.shadowOpacity = 1
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.868,
+                                              longitude: 151.2086,
+                                              zoom: 14)
+        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
+        
+        let marker = GMSMarker()
+        marker.position = camera.target
+        marker.snippet = "Hello World"
+        marker.appearAnimation = GMSMarkerAnimation.pop
+        marker.map = mapView
+        
+        cell.mapHolder.addSubview(mapView)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 316
+    }
+}
